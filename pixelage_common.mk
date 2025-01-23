2d02dc63ee7186ede452749e5c25b70201c4ac27
COSMIC <rayavanindra@gmail.com>
2025-01-23 17:44:43 +0530

    gs201: Project Pixelage bringup

--- pixelage_common.mk
+++ pixelage_common.mk
@@ -5,7 +5,7 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += device/google/gs201/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += device/google/gs201/overlay-pixelage
 
 # AiAi Config
 PRODUCT_COPY_FILES += \
