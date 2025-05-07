ionutsandroidbuilds/android_device_google_gs201

gs201: enable VoLTE, VoWiFi, Video Calling, VoNR, RCS & 5G for non-supported countries
Signed-off-by: Ionut Gherman <ghermanionut96@gmail.com>

device-common.mk
@@ -29,6 +29,8 @@ endif
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+TARGET_SYSTEM_PROP := device/google/zuma/system.prop
+
 # Set system properties identifying the chipset
 PRODUCT_VENDOR_PROPERTIES += ro.soc.manufacturer=Google
 PRODUCT_VENDOR_PROPERTIES += ro.soc.model=GS201

c0smic-Lab/device_google_gs201

device-common.mk
@@ -29,6 +29,8 @@ endif
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+TARGET_SYSTEM_PROP := device/google/gs201/system.prop
+
 # Set system properties identifying the chipset
 PRODUCT_VENDOR_PROPERTIES += ro.soc.manufacturer=Google
 PRODUCT_VENDOR_PROPERTIES += ro.soc.model=GS201